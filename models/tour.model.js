7c43dcbd222764bf09d2262695655f96d7897de6
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-24 15:40:26 -0700

    tour model update

--- models/tour.model.js
+++ models/tour.model.js
@@ -3,15 +3,52 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Tour name is required"],
+    trim: true,
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "Tour duration is required"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "Tour group size is required"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "Tour difficulty is required"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuatity: {
     type: Number,
     default: 4.5,
   },
   price: {
     type: Number,
     required: [true, "Tour price is required"],
+  },
+  priceDiscount: Number,
+  summery: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "A tour must have a summery"],
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a images cover"],
+  },
+  images: [String],
+  startDates: [Date],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
