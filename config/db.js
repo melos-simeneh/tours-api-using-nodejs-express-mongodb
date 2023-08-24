6991e0a267545e72c05c9fa94c80dec01d11eef1
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-24 13:59:00 -0700

    db config update

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config({ path: "./.env" });
 
 exports.connect = async () => {
   await mongoose
-    .connect("mongodb://localhost:27017/tours_db")
+    .connect(process.env.MONGO_DATABASE)
     .then(() => {
       console.log("Database Connected Successfully!");
     })
