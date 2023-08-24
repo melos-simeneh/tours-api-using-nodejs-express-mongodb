40460ff757636796c7867bfda794219cf7d5396b
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-24 14:43:29 -0700

    get all tours endpoint update

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,19 +1,32 @@
 const Tour = require("../models/tour.model");
-exports.getAllTours = (req, res) => {
-  res.status(200).json({ status: "success", data: {} });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res
+      .status(200)
+      .json({ status: "success", results: tours.length, data: { tours } });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Failed to get tours",
+      error: error.message,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
-    res
-      .status(201)
-      .json({
-        status: "success",
-        message: "Tour created",
-        data: { tour: newTour },
-      });
+    res.status(201).json({
+      status: "success",
+      message: "Tour created",
+      data: { tour: newTour },
+    });
   } catch (error) {
-    res.status(500).json({ status: "fail", message: "Failed to create tour" });
+    res.status(500).json({
+      status: "fail",
+      message: "Failed to create tour",
+      error: error.message,
+    });
   }
 };
