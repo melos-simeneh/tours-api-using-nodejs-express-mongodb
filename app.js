addb62f967e2ca814bbda3474c28a85600e2f994
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-24 10:36:24 -0700

    tour routes create

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const tourRoutes = require("./routes/tour.routes");
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
+
+//Routes
+app.use("/api/tours", tourRoutes);
 
 module.exports = app;
