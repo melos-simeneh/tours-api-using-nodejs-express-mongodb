af6f41ca330544fe15286fedb0e23940dc8b1886
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-25 09:02:14 -0700

    aliasing on get all tours

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -3,7 +3,9 @@ const tourController = require("../controllers/tour.controller");
 
 const router = express.Router();
 
-router.route("/top-5-cheap").get(tourController.aliasTopTours);
+router
+  .route("/top-5-cheap")
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
   .route("/")
