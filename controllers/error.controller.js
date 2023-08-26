d1f0c9114e2392f0866e814c2a451ba5365f2861
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-26 11:37:58 -0700

    error contoller update

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,16 +1,22 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
   if (process.env.NODE_ENV === "development") {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-      stack: err.stack,
-    });
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
+    sendErrorProd(err, res);
   }
 };
