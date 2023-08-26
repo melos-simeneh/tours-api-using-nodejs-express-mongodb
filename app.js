a56b19bc10e11926d639c214d5ebcf0727a47ce1
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-26 10:47:46 -0700

    error contoller create

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const tourRoutes = require("./routes/tour.routes");
 const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/error.controller");
 
 const app = express();
 
@@ -14,10 +15,6 @@ app.all("*", (req, res, next) => {
   next(new AppError(`api url ${req.url} not found`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
