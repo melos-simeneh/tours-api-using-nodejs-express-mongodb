cf66812595470269fe2abb81df0a604d4f861edb
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-26 11:58:51 -0700

    error contoller update

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -2,14 +2,29 @@ const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
     stack: err.stack,
   });
 };
+
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  //Operational,trusted error:send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //Programming or other unknown error: don't leak error details
+    //Log error
+    console.error("ERROR 🔥", err);
+
+    //send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
 };
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
