097901c9847fad2e5c8c99362ae24c9b68056a32
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-26 10:20:29 -0700

    error handling

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ app.use(express.json());
 //Routes
 app.use("/api/tours", tourRoutes);
 
-app.all("*", (req, res) => {
+app.all("*", (req, res, next) => {
   res.status(404).json({
     status: "fail",
     message: `api url ${req.url} is not found`,
   });
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
 });
 
 module.exports = app;
