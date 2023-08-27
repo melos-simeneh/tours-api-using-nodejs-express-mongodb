1b85df3df70046751cb051743d127721d95342d0
Melos <melos.simeneh@gmail.com>
2023-08-27 10:20:24 +0300

    cast error handling

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,3 +1,10 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Inavlid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -29,13 +36,13 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  const NODE_ENV = process.env.NODE_ENV;
-
+  const NODE_ENV = process.env.NODE_ENV.trim();
+  console.log(NODE_ENV === "production");
   if (NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (NODE_ENV === "production") {
-    console.log(process.env.NODE_ENV);
-
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (err.name === "CastError") error = handleCastErrorDB(err);
+    sendErrorProd(error, res);
   }
 };
