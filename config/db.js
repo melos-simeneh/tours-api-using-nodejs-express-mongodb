5b86106d028b03f561a853f6d3e0d776aa290c1a
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 09:01:18 -0700

    db config update

--- config/db.js
+++ config/db.js
@@ -3,13 +3,11 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
 exports.connect = async () => {
-  await mongoose
-    .connect(process.env.MONGO_DATABASE)
-    .then(() => {
-      console.log("Database Connected Successfully!");
-    })
-    .catch((error) => {
-      console.log("Connection to Database Failed!");
-      throw error;
-    });
+  await mongoose.connect(process.env.MONGO_DATABASE).then(() => {
+    console.log("Database Connected Successfully!");
+  });
+  // .catch((error) => {
+  //   console.log("Connection to Database Failed!");
+  //   throw error;
+  // });
 };
