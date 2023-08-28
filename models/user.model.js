7f856639d7a2c0ea7d6ab009b8adcda1d8fc4de4
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 10:31:43 -0700

    password hashing update

--- models/user.model.js
+++ models/user.model.js
@@ -37,8 +37,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  const salt = await bcrypt.genSalt(12);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
   next();
 });
