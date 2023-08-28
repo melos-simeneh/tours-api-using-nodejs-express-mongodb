ce6dc8022f680b5d4f7f5765708c39a3b01a63f0
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 09:49:55 -0700

    signup update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ const User = require("../models/user.model");
 const catchAsync = require("../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await Tour.create(req.body);
+  const newUser = await User.create(req.body);
   res.status(201).json({
     status: "success",
     message: "user created",
