926bc06eb45bc8aa556e98dc0946db9b6c54e00d
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 11:46:25 -0700

    login update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,13 +2,16 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
   const newUser = await User.create({ name, email, password, passwordConfirm });
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: "success",
     message: "user created",
@@ -28,6 +31,11 @@ exports.login = catchAsync(async (req, res, next) => {
 
   if (!user || !(await user.correctPassword(password, user.password)))
     return next(new AppError("Incorrect email or password", 401));
-  const token = "";
-  res.status(200).json({ status: "success", token });
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    message: "logged in successfully",
+    token,
+  });
 });
