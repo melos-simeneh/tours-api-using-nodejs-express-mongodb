54c9cd127f4843bdcd8fe7771678755b49ea8c1e
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 08:47:35 -0700

    unhadled rejection

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const mongo = require("./config/db");
 mongo.connect();
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  process.exit(1);
 });
