eb2b16ac4a084f6bb50cda06e8ae14d7e806b9a9
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 11:14:36 -0700

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.signup = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
@@ -17,3 +18,11 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.bosy;
+  if (!email || !password)
+    next(new AppError("Please provide email and passsword", 400));
+  const token = "";
+  res.status(200).json({ status: "success", token });
+};
