b118e261644a69223551ab3d10aec6c4a4bf2c2d
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 09:16:26 -0700

    user model update

--- models/user.model.js
+++ models/user.model.js
@@ -15,6 +15,15 @@ const userSchema = new mongoose.Schema({
     validator: [validator.isEmail, "Please provide a valid email"],
   },
   photo: String,
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+  },
 });
 
 const User = mongoose.model("User", userSchema);
