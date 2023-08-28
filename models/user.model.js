6680d80bd995c81eca22118f279554b78a2dcc5a
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-28 09:10:17 -0700

    user model update

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: [true, "User's name is is required"],
+    required: [true, "Please tell use your name"],
   },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: String,
 });
 
 const User = mongoose.model("User", userSchema);
