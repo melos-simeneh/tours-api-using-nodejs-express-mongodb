1e070ee10de75ddea3696cd56ff1afda99ec3068
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-29 10:26:00 -0700

    email sender

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = (options) => {
+  // transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.EMAIL_PASSWORD,
+    },
+  });
+
+  // email options
+  const mailOptions = {
+    from: "Melos",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  // email sender
+  transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
