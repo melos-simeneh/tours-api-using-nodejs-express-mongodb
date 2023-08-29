c14c8a77edddcb122055b3c035315f15edea67b3
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-29 09:09:25 -0700

    forgot password

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
-router.post("/forgetPassword", authController.forgotPassword);
+router.post("/forgotPassword", authController.forgotPassword);
 
 module.exports = router;
