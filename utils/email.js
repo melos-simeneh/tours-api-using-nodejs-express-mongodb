99f05275293d37e589cb99d95754f27177c0ec85
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-29 13:34:03 -0700

    send email update

--- utils/email.js
+++ utils/email.js
@@ -3,8 +3,9 @@ const nodemailer = require("nodemailer");
 const sendEmail = (options) => {
   // transporter
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAL_HOST,
+    host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.EMAIL_PASSWORD,
